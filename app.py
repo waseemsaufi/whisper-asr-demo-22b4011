d519cce7e7e43a62bb0a41666fb5b8243d967c1b
waseemsaufi <waseemsaufi@gmail.com>
2026-04-26 20:45:26 +0800

    changes

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def preprocess_audio(audio_path, speed_factor=1.0):
     - Noise reduction
     - Normalization
     """
-    y, sr = librosa.load(audio_path, sr=16000, backend="soundfile")
+    y, sr = librosa.load(audio_path, sr=16000)
 
     # --- Speed control ---
     if speed_factor != 1.0:
